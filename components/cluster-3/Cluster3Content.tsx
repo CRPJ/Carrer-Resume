0169da454847ca8162dcfcbf23f6635f6f2ecfc4
Auurumm <23.aurum.06@gmail.com>
2026-02-08 15:39:16 +0900

    Cluster3 포트폴리오 아카이빙 Channel 까지 시안 일치

--- components/cluster-3/Cluster3Content.tsx
+++ components/cluster-3/Cluster3Content.tsx
@@ -984,25 +984,11 @@ const Cluster3Content = () => {
 
       {/* Section 3: 포트폴리오 마케팅 Channel */}
       <section className="cluster3-section3">
-        {/* 플로팅 아이콘 - 로그인한 본인만 표시 */}
-        {session && isOwner && (
-          <div className="floating-icons" style={{ display: 'flex' }}>
-            <div className="edit-icon" onClick={() => handleEditClick(() => {
-              // 포트폴리오 아카이빙은 10개만 DB 저장
-              setEditingSection3Links([...portfolioArchives]);
-              setSection3ModalOpen(true);
-            })} style={{ cursor: 'pointer' }}>
-              <img src="/images/0/cluster 3/icon/Edit_Pencil_Line_01.png" alt="Edit" />
-            </div>
-            <div className="edit-icon search-icon">
-              <svg viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2">
-                <circle cx="11" cy="11" r="8" />
-                <path d="M21 21l-4.35-4.35" />
-              </svg>
-              <div className="tooltip">등록된 도움말이 없습니다</div>
-            </div>
-          </div>
-        )}
+        {/* 플로팅 아이콘 */}
+        <div className="floating-icons" style={{ display: 'flex' }}>
+          <img src="/images/0/cluster 3/icon -  modify.png" alt="Modify" style={{ width: '22px', height: '22px', objectFit: 'contain', cursor: 'pointer' }} />
+          <img src="/images/0/cluster 3/icon - help.png" alt="Help" style={{ width: '22px', height: '22px', objectFit: 'contain', cursor: 'pointer' }} />
+        </div>
         {/* 배경 이미지 */}
         <div className="section3-bg">
           <img src="/images/0/cluster 3/bg3.png" alt="Background" />
@@ -1010,15 +996,13 @@ const Cluster3Content = () => {
 
         <div className="section3-header">
           <div className="header-left">
-            <h2 className="subtitle">
-              <span className="title-text">
-                <img src="/images/0/cluster 3/icon/triangle-orange.svg" alt="triangle" className="triangle-icon" />
-                포트폴리오 아카이빙 Channel
-              </span>
-            </h2>
+          <h2 className="subtitle">
+            <img src="/images/0/cluster 3/polygon.png" alt="triangle" style={{ width: '32px', height: '32px', objectFit: 'contain', marginRight: '-24px' }} />
+            포트폴리오 아카이빙 Channel
+          </h2>
             <div className="header-sub">
               <span className="view-all" onClick={() => handleArrowClick('section3')} style={{ cursor: 'pointer' }}>
-                View All Bids <span className={`arrow-icon ${arrowAnimating === 'section3' ? 'arrow-bounce' : ''}`}></span>
+                View All Bids <img src="/images/0/cluster 3/arrow.png" alt="arrow" style={{ width: '14px', height: '14px', objectFit: 'contain', marginLeft: '4px' }} />
               </span>
             </div>
           </div>
